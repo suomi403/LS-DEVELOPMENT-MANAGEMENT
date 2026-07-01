9a556c3a48467c7768803cc688bdde2efe69f03a
suomi403 <lukeshiomi2012@gmail.com>
2026-07-01 14:02:28 +0900

    旧環境削除＆Next.js移管

--- suomi/suomi_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from .models import Task
-
-# Register your models here.
-admin.site.register(Task)
-admin.site.unregister(Group)
--- suomi/suomi_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuomiAppConfig(AppConfig):
-    name = 'suomi_app'
